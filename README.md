c3e37baf3968063f5d48402c76870ed2313d6a3a
igorb <igorb@192.168.1.4>
2016-11-27 22:57:09 +0100

    Create initial db migration and generate java db code.

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # Planitia
 This is another test project to see how new stuff working together ...
 
-Database access library (ActiveRecord)
-=======================================
+This is a JavaEE application with web interface written with Polymer web components.
+
+#Used development tools:
+- Apache Tomee server is used for development
+- Netbeans IDE for Java and JavaScript editing and debugging
+- Chrome browser with Netbeans connector for debugging support
+
+
+#Database access library (ActiveRecord)
 Add jooq-3.8.6.jar library to project dependency.
 http://bisaga.com/blog/programming/jooq-database-access-layer/
 
-JEE Resources :
-=================
+#JEE Resources :
 http://bisaga.com/blog/programming/tomee-java-ee-server-database-configuration/
 Add this resource to server configuration file:  conf/tomee.xml
 	<Resource id="jdbc/db" type="javax.sql.DataSource">
@@ -17,21 +23,19 @@ Add this resource to server configuration file:  conf/tomee.xml
 		password postgres
 	</Resource>	
 
-Database driver for Postgres:
-=================================
+#Database driver for Postgres:
 Add postgresql-9.4.1212.jar to tomee/lib folder.
 
-Database migrations with flyway:
-================================
+#Database migrations with flyway:
 http://bisaga.com/blog/programming/flyway-setup-inside-java-project/
 
-
-Setup project in netbeans : 
-============================
+#Setup project in netbeans : 
 http://bisaga.com/blog/programming/prepare-javaeejavascript-development-environment/
 
 
-You can find out more about this project on the blog: 
-======================================================
+#You can find out more about this project on the blog: 
 http://bisaga.com/blog/
 
+
+#Polymer
+npm i Polymer
--- resources/db/migrations/V201611270140__Initial_create.sql
+++ resources/db/migrations/V201611270140__Initial_create.sql
@@ -1,16 +1,66 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * Author:  igorb
  * Created: Nov 27, 2016
  */
 
-create table book (
+create table user_info (
     id serial not null,
-    title text not null,
+    external_id text not null,
+    user_name text not null,
+    is_active boolean,
+    email text,
+    created_on timestamp with time zone,
+    created_by text not null,
+    modified_on timestamp with time zone,
+    modified_by text,
+    constraint user_pk primary key (id)
+);
 
-    constraint book_pk primary key (id)
+create table task (
+    id serial not null,
+    code text not null,
+    summary text not null,
+    story integer, 
+    description text,
+    assigned_by integer,
+    assignee integer,
+    created_on timestamp with time zone,
+    created_by text not null,
+    modified_on timestamp with time zone,
+    modified_by text,
+    constraint task_pk primary key (id)
+);
+
+create table task_link (
+    id serial not null,
+    task_id integer not null,
+    subtask_id integer not null,
+    constraint task_link_pk primary key (id)
+);
+
+create table story (
+    id serial not null,
+    code text not null,
+    summary text not null,
+    description text,
+    author integer,
+    reporter integer,
+    epic integer, 
+    created_on timestamp with time zone,
+    created_by text not null,
+    modified_on timestamp with time zone,
+    modified_by text,
+    constraint story_pk primary key (id)
+);
+
+create table epic (
+    id serial not null,
+    code text not null,
+    summary text not null,
+    description text,
+    created_on timestamp with time zone,
+    created_by text not null,
+    modified_on timestamp with time zone,
+    modified_by text,
+    constraint epic_pk primary key (id)
 );
